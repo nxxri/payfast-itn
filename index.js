780f33978575492c280be7d290706e19ca162847
Noorain Dayer <nooraindayer@gmail.com>
2025-12-12 11:15:00 +0200

    Cors fix

--- index.js
+++ index.js
@@ -7,13 +7,20 @@ const crypto = require('crypto');
 const cors = require('cors');
 
 const app = express();
+// ===== FIX GLOBAL CORS (must be at the VERY TOP) =====
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*"); // or your URL only
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+    if (req.method === "OPTIONS") {
+        return res.sendStatus(200); // <-- CRITICAL
+    }
+
+    next();
+});
 
 // ========== MIDDLEWARE ==========
-app.use(cors({
-    origin: ['https://salwacollective.co.za', 'http://localhost:3000'],
-    methods: ['POST', 'GET'],
-    credentials: false
-}));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 
