615f786e7168c039b228ce2018e3fbba9ce6f372
Noorain Dayer <nooraindayer@gmail.com>
2026-01-22 09:26:12 +0200

    YOCO backend test

--- server.js
+++ server.js
@@ -6,25 +6,13 @@ const fetch = (...args) =>
 
 const admin = require('firebase-admin');
 
-const app = express(); // <-- you were missing this line
+const app = express();
 
-const firebaseConfig = {
-    type: process.env.FIREBASE_TYPE,
-    project_id: process.env.FIREBASE_PROJECT_ID,
-    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-    client_email: process.env.FIREBASE_CLIENT_EMAIL,
-    client_id: process.env.FIREBASE_CLIENT_ID,
-    auth_uri: 'https://accounts.google.com/o/oauth2/auth',
-    token_uri: 'https://oauth2.googleapis.com/token',
-    auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
-    client_x509_cert_url: `https://www.googleapis.com/robot/v1/metadata/x509/${process.env.FIREBASE_CLIENT_EMAIL}`
-};
-
-// Initialize Firebase
+// Firebase Admin
 admin.initializeApp({
-    credential: admin.credential.cert(firebaseConfig)
+    credential: admin.credential.cert(JSON.parse(process.env.FIREBASE_KEY))
 });
+firebaseConfig.private_key = firebaseConfig.private_key.replace(/\\n/g, '\n');
 
 const db = admin.firestore();
 
